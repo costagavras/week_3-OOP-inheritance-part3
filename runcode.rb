3d32b43f3b1f3d2d9fe077141631b8a9faf4b216
costagavras <cgavras@yahoo.ca>
2018-09-15 17:27:13 -0400

    final

---
+++ runcode.rb
@@ -0,0 +1,28 @@
+#comparable celestial bodies mass in M(Earth):
+# Sun: 333,000; Mercury: 0.055; Venus: 0.815; Earth: 1; Moon: 0.0123; Mars: 0.107; Jupiter: 318; Saturn: 95.2; Uranus: 14.6; Neptune: 17.2; Pluto: 0.002.
+
+require_relative("solar_system")
+
+#creating bodies and populating "bodies" array
+ar_bodies = []
+sun = Star.create("Sun", 333000, "G-type")
+ar_bodies << sun
+earth = Planet.new("Earth", 1, 24, 365.25)
+ar_bodies << earth
+moon = Moon.new("Moon", 0.0123, "Earth", 29.5)
+ar_bodies << moon
+puts ar_bodies.inspect
+
+#creating the Solar system instance
+solar_system = System.create("Solar",ar_bodies)
+puts solar_system.all.inspect
+
+#calculating the mass of the instance
+puts solar_system.total_mass
+
+#adding a new planet
+mars = Planet.new("Mars", 0.107, 24.66, 687)
+solar_system.add(mars)
+puts solar_system.all.inspect
+
+puts solar_system.total_mass
---
+++ solar_system.rb
@@ -0,0 +1,107 @@
+# we'll need a class to represent the solar system. Let's call it System, and give it an attribute bodies.
+# bodies will start as an empty array (ie. []).
+
+require "pry"
+
+
+
+class System
+
+
+
+  def initialize(name, bodies)
+    @@bodies = []
+    @name = name
+    @@bodies = bodies
+  end
+
+  def self.create(name, bodies)
+    new_system = System.new(name, bodies)
+    # bodies = [sun, earth, moon]
+    @@bodies = bodies
+    return new_system
+  end
+
+  # reader
+  def all
+    return @@bodies
+  end
+
+  def add(body)
+    puts "Adding a new body"
+# binding.pry
+    @@bodies << body
+  end
+
+  def total_mass
+    total = 0
+    @@bodies.each do |element|
+      total += element.mass
+    end
+    puts "Total mass of Solar system's bodies is now #{total} of Earth's mass."
+    return total
+  end
+
+end
+
+class Body < System
+
+  def initialize(name, mass)
+    @name = name
+    @mass = mass
+  end
+
+end
+
+class Star < Body
+
+  def initialize(name, mass, type)
+    super(name, mass)
+    @type = type
+  end
+
+  def self.create(name, mass, type)
+    new_star = Star.new(name, mass, type)
+  end
+
+  def mass
+    return @mass
+  end
+
+end
+
+class Planet < Body
+
+  def initialize(name, mass, day, year)
+    super(name, mass)
+    @day = day
+    @year = year
+  end
+
+  def self.create
+    new_planet = Planet.new(day, year)
+  end
+
+  def mass
+    return @mass
+  end
+
+end
+
+class Moon < Body
+
+  def initialize(name, mass, planet, month)
+    super(name, mass)
+    @planet = planet
+    @month = month
+  end
+
+  def self.create
+    new_moon = Moon.new(planet, month)
+  end
+
+  def mass
+    return @mass
+  end
+
+end
